b9b73dcf77484a14c610cdca31df8f362929c741
deepananbalagan <deepanbtechceg@gmail.com>
2025-04-14 10:31:29 +0200

    updated documentation endpoint

--- src/main/java/com/tib/ts/mod/feature/documentation/DocumentationController.java
+++ src/main/java/com/tib/ts/mod/feature/documentation/DocumentationController.java
@@ -3,12 +3,15 @@ package com.tib.ts.mod.feature.documentation;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  *@author Deepan Anbalagan
@@ -20,24 +23,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Documentation", description = "Get documentation about the service")
 class DocumentationController {
 	
-	/*
-	 * @GetMapping("/doc/api") public void getAPIDocs(HttpServletResponse response)
-	 * throws IOException { response.sendRedirect("/swagger-ui/index.html");
-	 * 
-	 * }
-	 */
-	
-	/*@GetMapping("/doc/api")
-	public String getAPIDocs() {
-		return "forward:/swagger-ui/index.html";
-	}
-	*/
-	
 	@GetMapping("/doc/api")
     @Operation(summary = "Get API Documentation", description = "Returns the API documentation link")
-    public ResponseEntity<Map<String, String>> showApiDocs() {
-        Map<String, String> apiDocs = new HashMap<>();
-        apiDocs.put("swagger-ui", "/swagger-ui/index.html");
-        return ResponseEntity.ok(apiDocs);
+    public String showApiDocs(HttpServletRequest request, HttpServletResponse response) {
+		String redirectURL = request.getContextPath() + "/swagger-ui/index.html";
+		response.setStatus(HttpStatus.MOVED_PERMANENTLY.value());
+        response.setHeader("Location", "/swagger-ui/index.html");
+        return redirectURL;
     }
 }

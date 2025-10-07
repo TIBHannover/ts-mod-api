aa2f6c99a365fcdd1a2a1f1e8988c044cc4a40c6
deepananbalagan <deepanbtechceg@gmail.com>
2025-10-07 12:28:54 +0200

    added explicit forwardedHeaderFilter

--- src/main/java/com/tib/ts/mod/config/WebConfig.java
+++ src/main/java/com/tib/ts/mod/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.tib.ts.mod.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -27,5 +29,10 @@ public class WebConfig implements WebMvcConfigurer{
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(contextInterceptor);
 	}
+	
+	@Bean
+    public ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
+    }
 
 }
